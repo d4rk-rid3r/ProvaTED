b383b41d228be80b76e2518929d8f0901525fe76
d4rk-rid3r <124785475+d4rk-rid3r@users.noreply.github.com>
2024-03-12 11:50:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let alunos = [
+    {
+        titulo: "Dom Casmurro",
+        paginas: 300,
+        genero: "clássico",
+    },
+    {
+        titulo: "IT a coisa",
+        paginas: 150,
+        genero: "terror",
+    },
+    {
+        titulo: "Jason",
+        paginas: 70,
+        genero: "terror",
+    },
+    {
+        titulo: "Chapéuzinho Vermelho",
+        paginas: 10,
+        genero: "clássico",
+    },
+    {
+        titulo: "Histórinhas de terror para crianãs",
+        paginas: 50,
+        genero: "clássico",
+    },
+]
+
+import function (CriarPilha,) from "./pilha.js"
+
---
+++ pilha.js
@@ -0,0 +1,20 @@
+export function CriarPilha (tamnanho=4){
+
+    return [...new alunos(tamanhoDaPilha)]
+}
+export function inserirPilha(pilha,item){
+
+    const spacePosition=pilha.indexOf(undefined)
+
+    if(spacePosition===1){
+        console.log("Não tem espaço")
+        return
+    }
+
+    for (let i = 0; i < alunos.length; i++) {
+        const inserir = alunos[i];
+        inserindo[i]=CriarPilha.pop[i]
+        console.log(inserindo)
+    }
+
+}
