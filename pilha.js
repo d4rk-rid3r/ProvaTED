6a7c8ccf6579c3506201df439134ce6539ba0f48
d4rk-rid3r <124785475+d4rk-rid3r@users.noreply.github.com>
2024-03-12 12:32:59 -0300

    pilha.js

--- pilha.js
+++ pilha.js
@@ -1,20 +1,37 @@
-export function CriarPilha (tamnanho=4){
-
-    return [...new alunos(tamanhoDaPilha)]
+export function criarPilha(tamanho) {
+    return new Array(tamanho)
 }
-export function inserirPilha(pilha,item){
 
-    const spacePosition=pilha.indexOf(undefined)
 
-    if(spacePosition===1){
-        console.log("Não tem espaço")
-        return
+export function inserirPilha(pilha, livro) {
+    for (let i = 0; i < pilha.length; i++) {
+        if (!pilha[i]) {
+            pilha[i] = livro;
+            console.log(`${livro.titulo} injetando item na pilha`);
+            return;
+        }
     }
+    console.log("A pilha está cheia.");
+}
 
-    for (let i = 0; i < alunos.length; i++) {
-        const inserir = alunos[i];
-        inserindo[i]=CriarPilha.pop[i]
-        console.log(inserindo)
+export function retirarPilha(pilha) {
+    for (let i = pilha.length - 1; i >= 0; i--) {
+        if (pilha[i]) {
+            console.log(`${pilha[i].titulo} removido da nossa pilha`);
+            pilha[i] = null;
+            return;
+        }
     }
+    console.log("A pilha está vazia.");
+}
 
+export function Existe(pilha, nome) {
+    for (let i = 0; i < pilha.length; i++) {
+        if (pilha[i] && pilha[i].titulo === nome) {
+            console.log(`O livro "${nome}" está na pilha.`);
+            return true;
+        }
+    }
+    console.log(`O livro "${nome}" não está na pilha.`);
+    return 0;
 }
